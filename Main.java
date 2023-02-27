f2c3c54a1b19b7f12d611e7778b7b36c38e21122
Jumppy1 <113559416+Jumppy1@users.noreply.github.com>
2023-02-27 19:36:04 +0000

    it reads the string and the length for the string.

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-import java.util.Scanner; 
 
 class Main {
   public static void main(String[] args) {
    
+    String name = "hello";
+    String name2 = "a";
     
-    
-    System.out.println("Enter a word: ");
+    int length = name.length();
+    int length2 = name2.length();
 
-    Scanner myScanner = new Scanner(System.in);
-    String UserInput = myScanner.next();
-
-    System.out.println(UserInput);
+    System.out.println(" \""+name+"\" the length is: " +length); 
+    System.out.println(" \""+name2+"\" the length is: " +length2); 
     
   }
 }
